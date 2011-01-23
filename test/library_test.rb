a185e47880bbe8d8ed0994bc0a2abb654bd94af8
Tim Morgan <tim@timmorgan.org>
2011-01-23 17:44:37 -0600

    New 'join' function in the library.

--- test/library_test.rb
+++ test/library_test.rb
@@ -10,4 +10,13 @@ class LibraryTest < Test::Unit::TestCase
   def test_exponentiation
     assert_equal "16", execute("2 ^ 4")
   end
+
+  def test_map
+    assert_equal "0\n2\n4\n6", execute("map [0 1 2 3],
+                                            [x] { x * 2 }")
+  end
+
+  def test_join
+    assert_equal "1,2,3,4", execute("join [1 2 3 4] ','")
+  end
 end
