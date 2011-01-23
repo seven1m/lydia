938d1e72845bdb4e73abb13ab648912cc3638973
Tim Morgan <tim@timmorgan.org>
2011-01-23 09:46:22 -0600

    Refactor Airball::Functions a bit.

--- airball/functions.rb
+++ airball/functions.rb
@@ -1,11 +1,15 @@
 module Airball
   module Functions
+    def define_function(name, args, &body)
+      @scope[name.to_s] = Function.new(args.map(&:to_s), &body)
+    end
+
     def build_functions
-      @scope["out"] = Function.new(["expr"]) do |scope, expr|
-        @scope["stdout"].write expr.to_s + "\n"
+      define_function :out, [:expr] do |scope, expr|
+        scope["stdout"].write expr.to_s + "\n"
       end
 
-      @scope["+"] = Function.new(["left", "right"]) do |scope, left, right|
+      define_function :+, [:left, :right] do |scope, left, right|
         if Airball::Int === left
           Int.new(left.val + right.val)
         elsif Airball::List === left
@@ -15,42 +19,42 @@ module Airball
         end
       end
 
-      @scope["-"] = Function.new(["left", "right"]) do |scope, left, right|
+      define_function :-, [:left, :right] do |scope, left, right|
         Int.new(left.val - right.val)
       end
 
-      @scope["*"] = Function.new(["left", "right"]) do |scope, left, right|
+      define_function :*, [:left, :right] do |scope, left, right|
         Int.new(left.val * right.val)
       end
 
-      @scope["/"] = Function.new(["left", "right"]) do |scope, left, right|
+      define_function :/, [:left, :right] do |scope, left, right|
         Int.new(left.val / right.val)
       end
 
       @scope["true"] = True.new
       @scope["false"] = False.new
 
-      @scope["=="] = Function.new(["left", "right"]) do |scope, left, right|
+      define_function :==, [:left, :right] do |scope, left, right|
         left.val == right.val ? True.new : False.new
       end
 
-      @scope[">"] = Function.new(["left", "right"]) do |scope, left, right|
+      define_function :>, [:left, :right] do |scope, left, right|
         left.val > right.val ? True.new : False.new
       end
 
-      @scope[">="] = Function.new(["left", "right"]) do |scope, left, right|
-        left.val > right.val ? True.new : False.new
+      define_function :>=, [:left, :right] do |scope, left, right|
+        left.val >= right.val ? True.new : False.new
       end
 
-      @scope["<"] = Function.new(["left", "right"]) do |scope, left, right|
+      define_function :<, [:left, :right] do |scope, left, right|
         left.val < right.val ? True.new : False.new
       end
 
-      @scope["<="] = Function.new(["left", "right"]) do |scope, left, right|
+      define_function :<=, [:left, :right] do |scope, left, right|
         left.val <= right.val ? True.new : False.new
       end
 
-      @scope["if"] = Function.new(["cond", "t", "f"]) do |scope, cond, t, f|
+      define_function :if, [:cond, :t, :f] do |scope, cond, t, f|
         if cond.val == false
           if Function === f
             f.call [], scope
@@ -66,21 +70,21 @@ module Airball
         end
       end
 
-      @scope["for"] = Function.new(["list", "func"]) do |scope, list, func|
+      define_function :for, [:list, :func] do |scope, list, func|
         list.vals.each do |val|
           func.call [val], scope
         end
       end
 
-      @scope["first"] = Function.new(["list"]) do |scope, list|
+      define_function :first, [:list] do |scope, list|
         list.vals.first
       end
 
-      @scope["rest"] = Function.new(["list"]) do |scope, list|
+      define_function :rest, [:list] do |scope, list|
         List.new(list.vals[1..-1])
       end
 
-      @scope["count"] = Function.new(["list"]) do |scope, list|
+      define_function :count, [:list] do |scope, list|
         Int.new(list.vals.length)
       end
     end
